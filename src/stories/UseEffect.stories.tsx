fc1f172e05cd339649050f7d585d30e5e8b3b6f5
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-12 16:33:51 +0300

    23.useEffect(setTimeout,setInterval)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -30,3 +30,27 @@ export const SimpleExample1 = () => {
         <button onClick={() => setFake(fake + 1)}>+</button>
     </>
 }
+export const SetTimeoutExample = () => {
+    console.log('SetTimeoutExample')
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+        // setTimeout(() => {
+        //     console.log('setTimeout')
+        //     document.title = counter.toString()
+        // }, 3000)
+        setInterval(() => {
+            console.log('tick' + counter)
+            setCounter(state => state + 1)
+        }, 1000)
+
+    }, [])
+
+    return <>
+        Hello,counter:{counter}-- fake:{fake}
+        {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
+    </>
+}
